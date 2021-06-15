b7b491dc6e882c8ba8a687bf47c9f5a55faa901b
andrew <andrew>
2021-06-15 14:02:38 -0400

    fix build

--- flake.nix
+++ flake.nix
@@ -4,13 +4,23 @@
   inputs = {
     naersk.url = "github:nmattia/naersk";
     utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
+  outputs = { self, nixpkgs, utils, naersk, rust-overlay }:
     utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages."${system}";
-        naersk-lib = naersk.lib."${system}";
+        overlays = [
+          rust-overlay.overlay
+          (self: super:
+            {
+              rustc = self.latest.rustChannels.nightly.rust;
+              cargo = self.latest.rustChannels.nightly.rust;
+            }
+          )
+        ];
+        pkgs = import nixpkgs { inherit overlays system; };
+        naersk-lib = pkgs.callPackage naersk {};
       in
       rec {
         packages.rnix-lsp = naersk-lib.buildPackage {
